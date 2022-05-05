71325fc3e6647118d99897aa27ed2f37851a7640
Livia-CA <liviacaetanodeandrade@gmail.com>
2022-05-05 12:28:46 -0400

    adiciona código da aula 04

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
-function onScroll() {
-  if (scrollY > 0) {
-    navigation.classList.add('scroll')
+window.addEventListener('scroll', onScroll)
+
+
+function onScroll(){
+  showNavOnScroll()
+  showBackToTopButtonOnScroll()
+}
+
+function showNavOnScroll(){
+  if (scrollY>0){
+   navigation.classList.add('scroll')
   } else {
     navigation.classList.remove('scroll')
+  }
+}
+
+function showBackToTopButtonOnScroll(){
+  if (scrollY>500){
+    backToTopButton.classList.add('show')
+  }else{
+    backToTopButton.classList.remove('show')
   }
 }
 
