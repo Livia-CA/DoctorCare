c97808eee53022369d723886072ba87569ff840e
Livia-CA <liviacaetanodeandrade@gmail.com>
2022-05-06 16:31:17 -0400

    adiciona código da aula 05

--- main.js
+++ main.js
@@ -1,33 +1,59 @@
-window.addEventListener('scroll', onScroll)
+window.addEventListener('scroll', onScroll);
 
+onScroll();
+function onScroll() {
+  showNavOnScroll();
+  showBackToTopButtonOnScroll();
 
-function onScroll(){
-  showNavOnScroll()
-  showBackToTopButtonOnScroll()
+  activateMenuAtCurrentSection(home);
+  activateMenuAtCurrentSection(services);
+  activateMenuAtCurrentSection(about);
+  activateMenuAtCurrentSection(contact);
 }
 
-function showNavOnScroll(){
-  if (scrollY>0){
-   navigation.classList.add('scroll')
-  } else {
-    navigation.classList.remove('scroll')
+function activateMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2;
+
+  const sectionTop = section.offsetTop;
+  const sectionHeight = section.offsetHeight;
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop;
+
+  const sectionEndsAt = sectionTop + sectionHeight;
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine;
+
+  const sectionBoundaries = sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine;
+
+  const sectionId = section.getAttribute('id');
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`);
+
+  menuElement.classList.remove('active');
+  if (sectionBoundaries) {
+    menuElement.classList.add('active');
   }
 }
 
-function showBackToTopButtonOnScroll(){
-  if (scrollY>500){
-    backToTopButton.classList.add('show')
-  }else{
-    backToTopButton.classList.remove('show')
+function showNavOnScroll() {
+  if (scrollY > 0) {
+    navigation.classList.add('scroll');
+  } else {
+    navigation.classList.remove('scroll');
+  }
+}
+
+function showBackToTopButtonOnScroll() {
+  if (scrollY > 550) {
+    backToTopButton.classList.add('show');
+  } else {
+    backToTopButton.classList.remove('show');
   }
 }
 
 function openMenu() {
-  document.body.classList.add('menu-expanded')
+  document.body.classList.add('menu-expanded');
 }
 
 function closeMenu() {
-  document.body.classList.remove('menu-expanded')
+  document.body.classList.remove('menu-expanded');
 }
 
 ScrollReveal({
@@ -35,12 +61,12 @@ ScrollReveal({
   distance: '30px',
   duration: 700
 }).reveal(`
-  #home,
-  #home img,
+  #home, 
+  #home img, 
   #home .stats, 
   #services,
   #services header,
-  #services .card,
-  #about,
-  #about header,
+  #services .card
+  #about, 
+  #about header, 
   #about .content`)
